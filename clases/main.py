d0faf0d1eb31755f84df82f59db782ad1f1098cd
emilianobocco <emilianobocco00@gmail.com>
2023-11-14 15:45:22 -0300

    funciones ins del upd socio test

--- clases/main.py
+++ clases/main.py
@@ -6,31 +6,51 @@ from extraviado import Extraviado
 from datetime import datetime, timedelta
 import sqlite3
 
-libro1 = Libro(123,"asd","desc",200)
-print(libro1.get_estado())
+#libro1 = Libro(123,"asd","desc",200)
+#print(libro1.get_estado())
 
 
-a = datetime.now()
-nuevafecha = a + timedelta(days=90)
-print(nuevafecha)
+#a = datetime.now()
+#nuevafecha = a + timedelta(days=90)
+#print(nuevafecha)
 
 
-#Ejemplo insertar un socio nuevo, seria una funcion agregar_socio() dentro de la clase Socio
+#Ejemplo insertar un socio nuevo
 
-conexion = sqlite3.connect("biblioteca.db")
-cursor = conexion.cursor()
-
-
-nro = int(input("Ingrese numero socio: "))
-nombre = input("Ingrese nombre socio: ")
-apellido = input("Ingrese apellido socio: ")
-
-
-socio = Socio(nro, nombre, apellido)
-
-cursor.execute("INSERT INTO socios (nro_socio, nombre, apellido) VALUES (?, ?, ?)",
+def insertar_socio(conexion, socio):
+    cursor = conexion.cursor()
+    cursor.execute("INSERT INTO socios (nro_socio, nombre, apellido) VALUES (?, ?, ?)",
                (socio.get_nro_socio(), socio.get_nombre(), socio.get_apellido()))
+    
+    conexion.commit()
 
-conexion.commit()
-cursor.close()
+def actualizar_socio(conexion, socio):
+    cursor = conexion.cursor()
+    cursor.execute("UPDATE socios SET nombre = ?, apellido = ? WHERE nro_socio = ?",
+                    (socio.get_nombre(), socio.get_apellido(), socio.get_nro_socio()))
+    conexion.commit()
+    print(f"Información del socio con número {socio.get_nro_socio()} actualizada correctamente.")
+    
+
+def eliminar_socio(conexion, nro_socio):
+    cursor = conexion.cursor()
+    cursor.execute("DELETE FROM socios WHERE nro_socio = ?", (nro_socio,))
+    conexion.commit()
+    print(f"Socio con número {nro_socio} eliminado correctamente.")
+    
+     
+conexion = sqlite3.connect("C:\\Users\\User\\OneDrive\\Documentos\\Facultad\\2023\\DAO\\Tp\\TP_DAO\\clases\\biblioteca.db")
+
+#nro = int(input("Ingrese numero socio: "))
+#nombre = input("Ingrese nombre socio: ")
+#apellido = input("Ingrese apellido socio: ")
+#nro_eliminado = int(input("Ingrese el nro de socio que desea eliminar: "))
+
+socio_actualizado = Socio(60, "Diego", "Maradona")
+
+#socio1 = Socio(nro, nombre, apellido)
+#insertar_socio(conexion, socio1)
+#eliminar_socio(conexion, nro_eliminado)
+actualizar_socio(conexion, socio_actualizado)
+
 conexion.close()
